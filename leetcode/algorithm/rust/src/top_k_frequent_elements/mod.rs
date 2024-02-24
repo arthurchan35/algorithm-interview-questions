1b79cad7feefea66f723e99d8e9b30010475ee19
Arthur Chen <arthur.chen@cloudkitchens.com>
2024-02-24 23:33:12 +0800

    top_k_frequent_elements solution 2: use another map to reverse map count to element instead of using structs. Faster on leetcode

--- leetcode/algorithm/rust/src/top_k_frequent_elements/mod.rs
+++ leetcode/algorithm/rust/src/top_k_frequent_elements/mod.rs
@@ -1,26 +1,8 @@
-use std::cmp::Ordering;
+use std::cmp::Reverse;
 use std::collections::HashMap;
 use std::collections::BinaryHeap;
+use std::vec;
 
-
-#[derive(Eq, PartialEq)]
-#[derive(Debug)]
-struct ElementCount {
-	element: i32,
-	count: u32
-}
-
-impl Ord for ElementCount {
-	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-		other.count.cmp(&self.count)
-	}
-}
-
-impl PartialOrd for ElementCount {
-	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		Some(self.cmp(other))
-	}
-}
 
 pub struct Solution {
 }
@@ -28,45 +10,64 @@ pub struct Solution {
 impl Solution {
 	pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
 
-		let mut element_counts_map: HashMap<i32, ElementCount> = HashMap::with_capacity(nums.len());
+		let mut element_counts_map: HashMap<i32, u32> = HashMap::with_capacity(nums.len());
 
 		for num in &nums {
 			let counts_ref = element_counts_map.get_mut(num);
 
 			match counts_ref {
-				Some(item) => {
-					item.count += 1;
+				Some(count) => {
+					*count += 1;
 				},
 				None => {
-					element_counts_map.insert(*num, ElementCount {element: *num, count: 1});
+					element_counts_map.insert(*num, 1);
 				},
 			}
 		}
 
-		let mut min_heap:BinaryHeap<ElementCount> = BinaryHeap::with_capacity(k as usize);
+		let mut min_heap:BinaryHeap<Reverse<u32>> = BinaryHeap::with_capacity(k as usize);
+		let mut count_to_elements_map: HashMap<u32, Vec<i32>> = HashMap::with_capacity(nums.len());
 
-		for value in element_counts_map.into_values() {
+		for (element, count) in element_counts_map.into_iter() {
+
+			match count_to_elements_map.get_mut(&count) {
+				Some(vector) => {
+					vector.push(element);
+				},
+				None => {
+					count_to_elements_map.insert(count, vec![element]);
+				},
+			}
 
 			if min_heap.len() < k as usize {
-				min_heap.push(value);
+				min_heap.push(Reverse(count));
 
 				continue;
 			}
 
 			if let Some(element_count) = min_heap.peek() {
-				if element_count.count < value.count {
+				if element_count.0 < count {
 					min_heap.pop();
-					min_heap.push(value);
+					min_heap.push(Reverse(count));
 				}
 			}
 		}
 
-		min_heap.iter().map(
-			|element_count| -> i32 {
-				element_count.element
-			}
-		).collect::<Vec<i32>>()
+		let mut return_vector:Vec<i32> = Vec::with_capacity(k as usize);
 
+		while !min_heap.is_empty() {
+			if let Some(count) = min_heap.pop() {
+
+				if let Some(vector) = count_to_elements_map.get_mut(&count.0) {
+
+					let last_element = vector.remove(vector.len() - 1);
+
+					return_vector.push(last_element);
+				}
+			}
+		}
+
+		return_vector
 	}
 }
 
@@ -77,11 +78,11 @@ mod tests {
 
 	#[test]
 	fn test() {
-		let result =  Solution::top_k_frequent(vec![4, 3, 2, 1, 3, 2, 1, 2, 1, 1], 3);
+		let result =  Solution::top_k_frequent(vec![1, 1, 1, 1, 3, 3, 5, 5, 73], 4);
 
 		println!("result: {:?}", result);
 
-		let expected_set:HashSet<i32> = vec![1,2,3].into_iter().collect();
+		let expected_set:HashSet<i32> = vec![1,3,5].into_iter().collect();
 
 		assert!(result.len() == expected_set.len());
 
