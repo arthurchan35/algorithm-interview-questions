2657d4f8c5fbc622e57ee0e5bb62641c245ad105
Arthur Chen <arthur.chen@cloudkitchens.com>
2024-03-16 20:14:37 +0800

    valid binary search tree -- pass down boundaries instead of returning them

--- leetcode/algorithm/java/src/valid/binary/search/tree/Solution.java
+++ leetcode/algorithm/java/src/valid/binary/search/tree/Solution.java
@@ -4,47 +4,30 @@ import util.tree.node.TreeNode;
 
 class Solution {
 
-	boolean valid = true;
-
-	public int[] treeRange(TreeNode curr) {
+	private boolean isValidBST(TreeNode curr, TreeNode lowerBound, TreeNode upperBound) {
 		if (curr == null) {
-			return null;
+			return true;
 		}
 
-		int[] currRange = new int[]{curr.val, curr.val};
-
-		int[] subRange = treeRange(curr.left);
-
-		if (subRange != null) {
-			if (subRange[0] < currRange[0]) {
-				currRange[0] = subRange[0];
-			}
-			if (subRange[1] >= curr.val) {
-				currRange[1] = subRange[1];
-				valid = false;
-			}
+		if (lowerBound != null && lowerBound.val >= curr.val) {
+			return false;
+		}
+		if (upperBound != null && upperBound.val <= curr.val) {
+			return false;
 		}
 
-		subRange = treeRange(curr.right);
-
-		if (subRange != null) {
-			if (subRange[0] <= curr.val) {
-				currRange[0] = subRange[0];
-				valid = false;
-			}
-
-			if (subRange[1] > currRange[1]) {
-				currRange[1] = subRange[1];
-			}
+		if (!isValidBST(curr.left, lowerBound, curr)) {
+			return false;
 		}
 
-		return currRange;
+		if (!isValidBST(curr.right, curr, upperBound)) {
+			return false;
+		}
+
+		return true;
 	}
 
 	public boolean isValidBST(TreeNode head) {
-
-		treeRange(head);
-
-		return valid;
+		return isValidBST(head, null, null);
 	}
 }
