0d61a308f5b449907fd4560a0d7f9b21735dbc10
Arthur Chen <arthur.chen@cloudkitchens.com>
2024-03-19 18:16:28 +0800

    ugly number is a number whose prime factors are in 2, 3, 5

---
+++ leetcode/algorithm/java/src/ugly/number/Solution.java
@@ -0,0 +1,24 @@
+package ugly.number;
+
+class Solution {
+	public boolean isUgly(int n) {
+
+		if (n < 1) {
+			return false;
+		}
+
+		while ((n & 1) == 0) {
+			n >>= 1;
+		}
+
+		while (n % 3 == 0) {
+			n /= 3;
+		}
+
+		while (n % 5 == 0) {
+			n /= 5;
+		}
+
+		return n == 1;
+	}
+}
