8f4137dbd19491ddfd4f3a4fad9aeec5e6377261
Arthur Chen <arthur.chen@cloudkitchens.com>
2024-03-21 23:25:12 +0800

    in cases like drawn pictures, the old solution recalculate 2, 3, 4, 5 multiple times, use dp to store calculated results to fix performance

--- leetcode/algorithm/java/src/course/schedule/Solution.java
+++ leetcode/algorithm/java/src/course/schedule/Solution.java
@@ -4,31 +4,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+
+/*
+ * 
+ * 			|______	6
+ * 			|		|
+ * 			5		|
+ * 			|______	4
+ * 			|		|
+ * 			3		|
+ * 			|______	2
+ * 			|		|
+ * 			1		|
+ * 			|		|
+ * 				0
+ */
 class Solution {
-	private boolean canFinish(int course, HashMap<Integer, ArrayList<Integer>> deps, HashSet<Integer> pathDups) {
+	private boolean canFinish(
+		int course,
+		HashMap<Integer, ArrayList<Integer>> deps,
+		HashSet<Integer> pathDups,
+		HashMap<Integer, Boolean> dp) { // dp is used for storing previously calculated results
+
+		Boolean calculatedResult = dp.get(course);
+
+		if (calculatedResult != null) {
+			return calculatedResult;
+		}
 
 		var nextCourses = deps.get(course);
 
 		if (nextCourses == null) {
+			dp.put(course, true);
 			return true;
 		}
 
 		if (pathDups.contains(course)) {
+			dp.put(course, false);
 			return false;
 		}
 
 		pathDups.add(course);
 
 		for (var nextCourse : nextCourses) {
-			boolean canFinish = canFinish(nextCourse, deps, pathDups);
+			boolean canFinish = canFinish(nextCourse, deps, pathDups, dp);
 
 			if (canFinish == false) {
+				dp.put(course, false);
 				return false;
 			}
 		}
 
 		pathDups.remove(course);
-
+		dp.put(course, true);
 		return true;
 	}
 
@@ -39,10 +67,13 @@ class Solution {
 		}
 
 		HashMap<Integer, ArrayList<Integer>> deps =
-			new HashMap<>(prerequisites.length * prerequisites[0].length);
+			new HashMap<>(prerequisites[0].length);
 
 		HashSet<Integer> pathDups =
-		new HashSet<>(prerequisites.length * prerequisites[0].length);
+			new HashSet<>(prerequisites[0].length);
+
+		HashMap<Integer, Boolean> dp =
+			new HashMap<>(prerequisites[0].length);
 
 		for (int i = 0; i < prerequisites.length; ++i) {
 			var list = deps.computeIfAbsent(prerequisites[i][1], key -> new ArrayList<>());
@@ -50,7 +81,7 @@ class Solution {
 		}
 
 		for (int course = 0; course < numCourses; ++course) {
-			boolean canFinish = canFinish(course, deps, pathDups);
+			boolean canFinish = canFinish(course, deps, pathDups, dp);
 
 			if (!canFinish) {
 				return false;
