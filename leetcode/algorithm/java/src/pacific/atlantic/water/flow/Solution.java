990d1973808d997384e5678e99ee6ad2270c9972
Arthur Chen <arthur.chen@cloudkitchens.com>
2024-03-22 21:03:38 +0800

    in current path has problem

--- leetcode/algorithm/java/src/pacific/atlantic/water/flow/Solution.java
+++ leetcode/algorithm/java/src/pacific/atlantic/water/flow/Solution.java
@@ -8,167 +8,199 @@ class Solution {
 	Boolean[][] pacificReachable;
 	Boolean[][] atlanticReachable;
 
-	boolean[][] currentPathVisited;
+	boolean[][] inCurrentPath;
 
-	private boolean pacificReachable(int[][] heights, int i, int j) {
-		if (i >= heights.length || j >= heights[0].length) {
-			return false;
+	private void printAtlanticReachable() {
+        for (int i = 0; i < atlanticReachable.length; ++i) {
+            for (int j = 0; j< atlanticReachable[0].length; ++j) {
+				boolean r = atlanticReachable[i][j];
+                System.out.print(r ? "t " : "f ");
+            }
+            System.out.println();
+        }
+    }
+
+	private void printPacificReachable() {
+        for (int i = 0; i < pacificReachable.length; ++i) {
+            for (int j = 0; j< pacificReachable[0].length; ++j) {
+				boolean r = pacificReachable[i][j];
+                System.out.print(r ? "t " : "f ");
+            }
+            System.out.println();
+        }
+    }
+
+	private void atlanticReachable(int[][] heights, int i, int j) {
+		if (atlanticReachable[i][j] != null) {
+			return;
 		}
 
-		if (pacificReachable[i][j] != null) {
-			return pacificReachable[i][j];
+		if (inCurrentPath[i][j]) {
+			return;
 		}
 
-		if (currentPathVisited[i][j]) {
-			return false;
+		if (i == heights.length - 1 || j == heights[0].length - 1) {
+			atlanticReachable[i][j] = true;
+			return;
 		}
 
-		// when a place is not determined, and asks for a result from next slot
-		// and if next is also not determined, the next one may asks for a result from previous one
-		currentPathVisited[i][j] = true;
+		inCurrentPath[i][j] = true;
 
-		boolean upper = pacificReachable(heights, i - 1, j);
-
-		if (upper) {
-			if (heights[i][j] >= heights[i - 1][j]) {
-				pacificReachable[i][j] = true;
-
-				// backtracking, when current place is returned, it defintely
-				// will have a result, clear current visited.
-				currentPathVisited[i][j] = false;
-				return true;
-			}
+		if (i == 3 && j == 27) {
+			System.out.println("entering i = 3, j = 27");
+			System.out.println("in current path left = " + inCurrentPath[i][j - 1]);
+			System.out.println("in current path upper = " + inCurrentPath[i - 1][j]);
+			System.out.println("in current path right = " + inCurrentPath[i][j + 1]);
+			System.out.println("in current path lower = " + inCurrentPath[i + 1][j]);
+			System.out.println("atlanticReachab left = " + atlanticReachable[i][j - 1]);
+			System.out.println("atlanticReachab upper = " + atlanticReachable[i - 1][j]);
+			System.out.println("atlanticReachab right = " + atlanticReachable[i][j + 1]);
+			System.out.println("atlanticReachab lower = " + atlanticReachable[i + 1][j]);
 		}
 
-		boolean left = pacificReachable(heights, i, j - 1);
+		if (heights[i][j] >= heights[i + 1][j] && !inCurrentPath[i + 1][j]) {
+			atlanticReachable(heights, i + 1, j);
 
-		if (left) {
-			if (heights[i][j] >= heights[i][j - 1]) {
-				pacificReachable[i][j] = true;
+			if (atlanticReachable[i + 1][j]) {
+				atlanticReachable[i][j] = true;
 
 				// backtracking
-				currentPathVisited[i][j] = false;
-				return true;
+				inCurrentPath[i][j] = false;
+				return;
 			}
 		}
 
-		boolean lower = pacificReachable(heights, i + 1, j);
+		if (heights[i][j] >= heights[i][j + 1] && !inCurrentPath[i][j + 1]) {
+			atlanticReachable(heights, i, j + 1);
 
-		if (lower) {
-			if (heights[i][j] >= heights[i + 1][j]) {
-				pacificReachable[i][j] = true;
+			if (atlanticReachable[i][j + 1]) {
+				atlanticReachable[i][j] = true;
 
 				// backtracking
-				currentPathVisited[i][j] = false;
-				return true;
+				inCurrentPath[i][j] = false;
+				return;
 			}
 		}
 
-		boolean right = pacificReachable(heights, i, j + 1);
+		if (i > 0 && heights[i][j] >= heights[i - 1][j] && !inCurrentPath[i - 1][j]) {
+			atlanticReachable(heights, i - 1, j);
 
-		if (right) {
-			if (heights[i][j] >= heights[i][j + 1]) {
-				pacificReachable[i][j] = true;
+			if (atlanticReachable[i - 1][j]) {
+				atlanticReachable[i][j] = true;
 
 				// backtracking
-				currentPathVisited[i][j] = false;
-				return true;
+				inCurrentPath[i][j] = false;
+				return;
 			}
 		}
 
+		if (j > 0 && heights[i][j] >= heights[i][j - 1] && !inCurrentPath[i][j - 1]) {
+			atlanticReachable(heights, i, j - 1);
+
+			if (atlanticReachable[i][j - 1]) {
+				atlanticReachable[i][j] = true;
+
+				// backtracking
+				inCurrentPath[i][j] = false;
+				return;
+			}
+		}
+		
 		// backtracking
-		currentPathVisited[i][j] = false;
-		return false;
+		inCurrentPath[i][j] = false;
+		atlanticReachable[i][j] = false;
 	}
 
-	private boolean atlanticReachable(int[][] heights, int i, int j) {
-		if (i < 0 || j < 0) {
-			return false;
+	private void pacificReachable(int[][] heights, int i, int j) {
+		if (pacificReachable[i][j] != null) {
+			return;
 		}
 
-		if (atlanticReachable[i][j] != null) {
-			return atlanticReachable[i][j];
+		if (inCurrentPath[i][j]) {
+			return;
 		}
 
-		if (currentPathVisited[i][j]) {
-			return false;
+		if (i == 0 || j == 0) {
+			pacificReachable[i][j] = true;
+			return;
+		}
+
+		inCurrentPath[i][j] = true;
+
+		if (heights[i][j] >= heights[i - 1][j] && !inCurrentPath[i - 1][j]) {
+			pacificReachable(heights, i - 1, j);
+
+			if (pacificReachable[i - 1][j]) {
+				pacificReachable[i][j] = true;
+
+				// backtracking
+				inCurrentPath[i][j] = false;
+				return;
+			}
+		}
+
+		if (heights[i][j] >= heights[i][j - 1] && !inCurrentPath[i][j - 1]) {
+			pacificReachable(heights, i, j - 1);
+
+			if (pacificReachable[i][j - 1]) {
+				pacificReachable[i][j] = true;
+
+				// backtracking
+				inCurrentPath[i][j] = false;
+				return;
+			}
+		}
+
+		if (i < heights.length - 1 && heights[i][j] >= heights[i + 1][j] && !inCurrentPath[i + 1][j]) {
+			pacificReachable(heights, i + 1, j);
+
+			if (pacificReachable[i + 1][j]) {
+				pacificReachable[i][j] = true;
+
+				// backtracking
+				inCurrentPath[i][j] = false;
+				return;
+			}
+		}
+
+		if (j < heights[0].length - 1 && heights[i][j] >= heights[i][j + 1] && !inCurrentPath[i][j + 1]) {
+			pacificReachable(heights, i, j + 1);
+
+			if (pacificReachable[i][j + 1]) {
+				pacificReachable[i][j] = true;
+
+				// backtracking
+				inCurrentPath[i][j] = false;
+				return;
+			}
 		}
 
 		// backtracking
-		currentPathVisited[i][j] = true;
-
-		boolean lower = atlanticReachable(heights, i + 1, j);
-
-		if (lower) {
-			if (heights[i][j] >= heights[i + 1][j]) {
-				atlanticReachable[i][j] = true;
-
-				currentPathVisited[i][j] = false;
-				return true;
-			}
-		}
-
-		boolean right = atlanticReachable(heights, i, j + 1);
-
-		if (right) {
-			if (heights[i][j] >= heights[i][j + 1]) {
-				atlanticReachable[i][j] = true;
-
-				currentPathVisited[i][j] = false;
-				return true;
-			}
-		}
-
-		boolean upper = atlanticReachable(heights, i - 1, j);
-
-		if (upper) {
-			if (heights[i][j] >= heights[i - 1][j]) {
-				pacificReachable[i][j] = true;
-
-				currentPathVisited[i][j] = false;
-				return true;
-			}
-		}
-
-		boolean left = atlanticReachable(heights, i, j - 1);
-
-		if (left) {
-			if (heights[i][j] >= heights[i][j - 1]) {
-				atlanticReachable[i][j] = true;
-
-				currentPathVisited[i][j] = false;
-				return true;
-			}
-		}
-
-		currentPathVisited[i][j] = false;
-		return false;
+		inCurrentPath[i][j] = false;
+		pacificReachable[i][j] = false;
 	}
 
 	public List<List<Integer>> pacificAtlantic(int[][] heights) {
 		pacificReachable = new Boolean[heights.length][heights[0].length];
 		atlanticReachable = new Boolean[heights.length][heights[0].length];
-		currentPathVisited = new boolean[heights.length][heights[0].length];
+		inCurrentPath = new boolean[heights.length][heights[0].length];
 
 		List<List<Integer>> results = new ArrayList<>(heights.length);
 
-		for (int j = 0; j < heights[0].length; ++j) {
-			pacificReachable[0][j] = true;
-			atlanticReachable[heights.length - 1][j] = true;
-		}
-		for (int i = 0; i < heights.length; ++i) {
-			pacificReachable[i][0] = true;
-			atlanticReachable[i][heights[0].length - 1] = true;
-		}
-
 		for (int i = 0; i < heights.length; ++i) {
 			for (int j = 0; j < heights[0].length; ++j) {
-				if (pacificReachable(heights, i, j) && atlanticReachable(heights, i, j)) {
+				pacificReachable(heights, i, j);
+				atlanticReachable(heights, i, j);
+
+				if (pacificReachable[i][j] && atlanticReachable[i][j]) {
 					results.add(List.of(i, j));
 				}
 			}
 		}
 
+		printAtlanticReachable();
+		System.out.println("------------------------------------------------------------------------");
+		printPacificReachable();
 		return results;
 	}
 }
