5df615cfe88e0735d0d57243733cff99d10d9b2d
Arthur Chen <arthur.chen@cloudkitchens.com>
2024-03-28 14:08:47 +0800

    with memo

--- leetcode/algorithm/java/src/maximal/square/Solution.java
+++ leetcode/algorithm/java/src/maximal/square/Solution.java
@@ -1,6 +1,10 @@
 package maximal.square;
 
+import java.util.Arrays;
+
 class Solution {
+
+	int[][] maximalSquare;
 
 	private int expandMaximalSquare(int i, int j, char[][] matrix) {
 
@@ -8,30 +12,46 @@ class Solution {
 			return 0;
 		}
 
+		if (maximalSquare[i][j] > 0) {
+			return maximalSquare[i][j];
+		}
+
 		if (i == matrix.length - 1 || j == matrix[0].length - 1) {
-			return matrix[i][j] == '1' ? 1 : 0;
+			maximalSquare[i][j] = matrix[i][j] == '1' ? 1 : 0;
+			return maximalSquare[i][j];
 		}
 
 		int subLength = expandMaximalSquare(i + 1, j + 1, matrix);
 
 		if (subLength == 0) {
-			return matrix[i][j] == '1' ? 1 : 0;
+			maximalSquare[i][j] = matrix[i][j] == '1' ? 1 : 0;
+
+			return maximalSquare[i][j];
 		}
 
 		for (int l = 1; l <= subLength; ++l) {
 			if (matrix[l + i][j] == '0') {
-				return l;
+				maximalSquare[i][j] = l;
+				return maximalSquare[i][j];
 			}
 			if (matrix[i][l + j] == '0') {
-				return l;
+				maximalSquare[i][j] = l;
+				return maximalSquare[i][j];
 			}
 
 		}
 
-		return subLength + 1;
+		maximalSquare[i][j] = subLength + 1;
+		return maximalSquare[i][j];
 	}
 
 	public int maximalSquare(char[][] matrix) {
+
+		maximalSquare = new int[matrix.length][matrix[0].length];
+
+		for (int i = 0; i < matrix.length; ++i) {
+			Arrays.fill(maximalSquare[i], -1);
+		}
 
 		int maxLength = 0;
 
